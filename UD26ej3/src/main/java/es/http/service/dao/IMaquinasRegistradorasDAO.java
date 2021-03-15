282b1611a133b193ee0bf01d23f9fb6fe3a97157
Civerneitor <sergio_castilla_13@hotmail.com>
2021-03-15 16:53:56 +0100

    Delete UD26ej3 directory

--- UD26ej3/src/main/java/es/http/service/dao/IMaquinasRegistradorasDAO.java
+++
@@ -1,9 +0,0 @@
-package es.http.service.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import es.http.service.dto.MaquinasRegistradoras;
-
-
-public interface IMaquinasRegistradorasDAO extends JpaRepository<MaquinasRegistradoras, Integer>{
-	
-}
